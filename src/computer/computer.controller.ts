cadfacd9c6b2cbdf2df6e9652582f3423ba3138b
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2023-07-31 13:54:44 +0200

    Connect disk service to computer controller to share data

--- src/computer/computer.controller.ts
+++ src/computer/computer.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { CpuService } from 'src/cpu/cpu.service';
+import { DiskService } from 'src/disk/disk.service';
 
 @Controller('computer')
 export class ComputerController {
-  constructor(private cpuService: CpuService) {}
+  constructor(
+    private cpuService: CpuService,
+    private diskService: DiskService,
+  ) {}
 
   @Get()
   getComputerRunning() {
-    return this.cpuService.compute(20);
+    return [this.diskService.getData(), 'and', this.cpuService.compute(20)];
   }
 }
